e7a77a330b60a87deb4785991f952d08128d439c
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-02-22 21:48:08 +0200

    add react-select

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import {findDOMNode} from 'react-dom'
 import ArticleList from './components/article-list'
 import Chart from './components/chart'
 import UserForm from './components/user-form'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
 
 class App extends Component {
     static propTypes = {
 
     };
 
+    state = {
+        selected: null
+    }
+
     render() {
+        const {articles} = this.props
+        const options = articles.map(article => ({
+            label: article.title,
+            value: article.id
+        }))
         return (
             <div>
                 <UserForm />
-                <ArticleList articles = {this.props.articles} ref = {this.setListRef}/>
-                <Chart articles = {this.props.articles}/>
+                <Select options = {options} value = {this.state.selected} onChange = {this.handleSelect} multi/>
+                <ArticleList articles = {articles} ref = {this.setListRef}/>
+                <Chart articles = {articles}/>
             </div>
         )
     }
+
+    handleSelect = selected => this.setState({ selected })
 
     setListRef = ref => {
         this.listRef = ref
