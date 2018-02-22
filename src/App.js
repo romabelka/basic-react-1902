a3bf825001422828d1a5ae24faf0ab658ae99c71
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-02-22 20:19:07 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default App
