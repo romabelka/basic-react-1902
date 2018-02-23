68884f002f54bf32ffc6c4b4401c18f78596d171
Dmitrii Lazucov <palzuncoff@MacBook-Dmitrii.local>
2018-02-23 19:02:22 +0200

    Change Calendar view from 'Range with two inputs' to 'Select a range of days'

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
-import moment from 'moment';
 import Helmet from 'react-helmet';
 
-import DayPickerInput from 'react-day-picker/DayPickerInput';
+import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css'
-
-import { formatDate, parseDate } from 'react-day-picker/moment';
 
 class Calendar extends Component {
   state = {
@@ -13,105 +10,70 @@ class Calendar extends Component {
     to: undefined,
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
+  handleDayClick = day => {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
   }
 
-  focusTo() {
-    this.timeout = setTimeout(() => this.to.getInput().focus(), 0);
-  }
-
-  showFromMonth() {
-    const { from, to } = this.state;
-    if (!from) {
-      return;
-    }
-    if (moment(to).diff(moment(from), 'months') < 2) {
-      this.to.getDayPicker().showMonth(from);
-    }
-  }
-
-  handleFromChange = from => {
-    // Change the from date and focus the "to" input field
-    this.setState({ from }, () => {
-      if (!this.state.to) {
-        this.focusTo();
-      }
-    });
-  }
-
-  handleToChange = to => {
-    this.setState({ to }, this.showFromMonth);
+  handleResetClick =() => {
+    this.setState({
+        from: undefined,
+        to: undefined,
+      });
   }
 
   render() {
     const { from, to } = this.state;
     const modifiers = { start: from, end: to };
     return (
-      <div className="InputFromTo">
-        <DayPickerInput
-          value={from}
-          placeholder="From"
-          format="LL"
-          formatDate={formatDate}
-          parseDate={parseDate}
-          dayPickerProps={{
-            selectedDays: [from, { from, to }],
-            disabledDays: { after: to },
-            toMonth: to,
-            modifiers,
-            numberOfMonths: 2,
-          }}
-          onDayChange={this.handleFromChange}
-        />{' '}
-        —{' '}
-        <span className="InputFromTo-to">
-          <DayPickerInput
-            ref={el => (this.to = el)}
-            value={to}
-            placeholder="To"
-            format="LL"
-            formatDate={formatDate}
-            parseDate={parseDate}
-            dayPickerProps={{
-              selectedDays: [from, { from, to }],
-              disabledDays: { before: from },
-              modifiers,
-              month: from,
-              fromMonth: from,
-              numberOfMonths: 2,
-            }}
-            onDayChange={this.handleToChange}
-          />
-        </span>
+      <div className="RangeExample">
+        <p>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+            to &&
+            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+          {from &&
+            to && (
+              <button className="link" onClick={this.handleResetClick}>
+                Reset
+              </button>
+            )}
+        </p>
+        <DayPicker
+          className="Selectable"
+          numberOfMonths={this.props.numberOfMonths}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
         <Helmet>
           <style>{`
-  .InputFromTo .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
     background-color: #f0f8ff !important;
     color: #4a90e2;
   }
-  .InputFromTo .DayPicker-Day {
+  .Selectable .DayPicker-Day {
     border-radius: 0 !important;
   }
-  .InputFromTo .DayPicker-Day--start {
+  .Selectable .DayPicker-Day--start {
     border-top-left-radius: 50% !important;
     border-bottom-left-radius: 50% !important;
   }
-  .InputFromTo .DayPicker-Day--end {
+  .Selectable .DayPicker-Day--end {
     border-top-right-radius: 50% !important;
     border-bottom-right-radius: 50% !important;
-  }
-  .InputFromTo .DayPickerInput-Overlay {
-    width: 550px;
-  }
-  .InputFromTo-to .DayPickerInput-Overlay {
-    margin-left: -198px;
   }
 `}</style>
         </Helmet>
       </div>
     );
   }
+}
+
+Calendar.defaultProps = {
+  numberOfMonths: 2,
 }
 
 export default Calendar
