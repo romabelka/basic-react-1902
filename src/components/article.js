d34b563f69e961497b1e4ada05fc8f1b7d82b16b
boombarashk <boombarashk@yandex.ru>
2018-02-25 10:42:32 +0300

    HW-1

--- src/components/article.js
+++ src/components/article.js
@@ -1,26 +1,34 @@
 import React, {PureComponent} from 'react'
 import PropTypes from 'prop-types'
+import CommentsList from './comment-list'
+import tumbler from '../decorators/tumbler'
 
 class Article extends PureComponent {
     render() {
-        const { isOpen, article, onButtonClick } = this.props
+        const { isOpen, article, onButtonClick, toggleStatus, toggleDepend  } = this.props
         console.log('---', 1)
         return (
             <div>
                 <h2>
                     {article.title}
-                    <button onClick={() => onButtonClick(article.id)}>{isOpen ? 'close' : 'open'}</button>
+                    <button onClick={() => onButtonClick(article.id)}>{String.fromCharCode(isOpen ? 0x25b6: 0x25bc)}</button>
                 </h2>
-                {isOpen && getBody(article)}
+                {isOpen && getBody(article, toggleStatus, toggleDepend)}
             </div>
         )
     }
 }
 
-function getBody(article) {
+function getBody(article, flag, toggleDepend) {
+    let commentsToggleBtn
+    if (typeof article.comments !== 'undefined') {
+        commentsToggleBtn = <button onClick={()=> toggleDepend(flag) }>{flag ? 'hide comments' : 'open  comments'}</button>
+    }
     return (
         <section>
             {article.text}
+            {commentsToggleBtn}
+            <CommentsList comments = {article.comments} toggleFlag = {flag}/>
         </section>
     )
 }
@@ -32,7 +40,11 @@ Article.propTypes = {
         title: PropTypes.string.isRequired,
         text: PropTypes.string
     }).isRequired,
-    onButtonClick: PropTypes.func
+    onButtonClick: PropTypes.func,
+
+    //from tumbler decorator
+    toggleStatus: PropTypes.bool,
+    toggleDepend: PropTypes.func
 }
 
-export default Article
+export default tumbler(Article)
