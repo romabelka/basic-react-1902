c85aaf7aa49e7c730b61423b0e4e88467dbacfef
Denis Komyagin <blackpirates@mail.ru>
2018-02-27 23:23:32 +0300

    add comment list open animation

--- src/components/comment-list/index.js
+++ src/components/comment-list/index.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import Comment from './comment'
-import toggleOpen from '../decorators/toggleOpen'
+import Comment from './../comment'
+import toggleOpen from '../../decorators/toggleOpen'
+import CSSTransition from 'react-addons-css-transition-group'
+import "./style.css"
 
 class CommentList extends Component {
     static defaultProps = {
@@ -18,10 +20,17 @@ class CommentList extends Component {
     render() {
         const {isOpen, toggleOpen} = this.props
         const text = isOpen ? 'hide comments' : 'show comments'
+
         return (
             <div>
                 <button className="test__comment-list--button" onClick={toggleOpen}>{text}</button>
-                {this.getBody()}
+                <CSSTransition
+                    transitionName="comment-list"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={300}
+                    component="div">
+                    {this.getBody()}
+                </CSSTransition>
             </div>
         )
     }
@@ -32,7 +41,7 @@ class CommentList extends Component {
 
         const body = comments.length ? (
             <ul>
-                {comments.map(comment => <li key = {comment.id}><Comment comment = {comment} /></li>)}
+                {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
             </ul>
         ) : <h3>No comments yet</h3>
 
