753df636187d5b746ca3cc1c2927c7d66fb77580
boombarashk <boombarashk@yandex.ru>
2018-03-10 12:06:33 +0300

    HW-4 3:14

---
+++ src/middlewares/randomid.js
@@ -0,0 +1,16 @@
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+export default store => next => action => {
+    console.log('RANDOMIZER ' + shuffle((new Date).toLocaleString('en-US').split('').filter((symbol)=>{return symbol != ' '})).join(''))
+    next(action)
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducer from '../reducer'
 import logger from '../middlewares/logger'
+import randomstringid from '../middlewares/randomid'
 
 const enhancer = applyMiddleware(logger)
 
-const store = createStore(reducer, enhancer)
+const saidtodo = applyMiddleware(randomstringid)
+
+const store = createStore(reducer, enhancer, saidtodo)
 
 //dev only, no need in prod
 window.store = store
