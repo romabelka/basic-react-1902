482127fceefebb126f61b8b94b98faca0bf252d2
boombarashk <boombarashk@yandex.ru>
2018-03-14 07:30:35 +0300

    HW-5 0.01

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,6 +1,14 @@
 import { ADD_COMMENT } from '../constants'
 import {normalizedComments} from '../fixtures'
 import {arrToMap} from './utils'
+/*
+
+const CommemtsRecord = Record({
+    id: null,
+    user: null,
+    text: null,
+    loading: false
+})*/
 
 export default (state = arrToMap(normalizedComments), action) => {
     const { type, payload, randomId } = action
