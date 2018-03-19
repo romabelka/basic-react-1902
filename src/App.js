52ab1f30882c7ae0d113d787b41180d13b1f5d88
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-03-19 15:58:01 +0200

    add HW6

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UserForm from './components/user-form'
 import Filters from './components/filters'
 import Counter from './components/counter'
 import 'react-select/dist/react-select.css'
+import CommentsPage from './components/routes/comments-page'
 
 class App extends Component {
     static propTypes = {
@@ -19,12 +20,14 @@ class App extends Component {
                     <li><NavLink to = "/counter" activeStyle = {{ color: 'red' }}>counter</NavLink></li>
                     <li><NavLink to = "/filters" activeStyle = {{ color: 'red' }}>filters</NavLink></li>
                     <li><NavLink to = "/articles" activeStyle = {{ color: 'red' }}>articles</NavLink></li>
+                    <li><NavLink to = "/comments/1" activeStyle = {{ color: 'red' }}>comments</NavLink></li>
                 </ul>
                 <Switch>
                     <Route path = "/counter" component = {Counter} exact />
                     <Route path = "/filters" component = {Filters} />
                     <Route path = "/articles/new" render = {() => <h1>New Article Form</h1>} />
                     <Route path = "/articles" component = {ArticlesPage} />
+                    <Route path = "/comments" component = {CommentsPage} />
                     <Route path = "*" render = {() => <h1>Not Found Page</h1>} />
                 </Switch>
             </div>
--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,6 +1,6 @@
-import { ADD_COMMENT, LOAD_ARTICLE_COMMENTS, SUCCESS } from '../constants'
-import {Record, OrderedMap} from 'immutable'
+import { ADD_COMMENT, LOAD_ARTICLE_COMMENTS, LOAD_COMMENTS_FOR_PAGE, START, SUCCESS } from '../constants'
 import {arrToMap} from './utils'
+import {OrderedMap, Map, Record} from 'immutable'
 
 const CommentRecord = Record({
     id: null,
@@ -9,7 +9,9 @@ const CommentRecord = Record({
 })
 
 const ReducerState = Record({
-    entities: new OrderedMap({})
+    entities: new OrderedMap({}),
+    pagination: new Map({}),
+    total: null
 })
 
 export default (state = new ReducerState(), action) => {
@@ -25,7 +27,16 @@ export default (state = new ReducerState(), action) => {
         case LOAD_ARTICLE_COMMENTS + SUCCESS:
             return state.mergeIn(['entities'], arrToMap(response, CommentRecord))
 
-        default:
+        case LOAD_COMMENTS_FOR_PAGE + START:
+            return state.setIn(['pagination', payload.page, 'loading'], true)
+
+        case LOAD_COMMENTS_FOR_PAGE + SUCCESS:
             return state
+                .set('total', response.total)
+                .mergeIn(['entities'], arrToMap(response.records, CommentRecord))
+                .setIn(['pagination', payload.page, 'ids'], response.records.map(comment => comment.id))
+                .setIn(['pagination', payload.page, 'loading'], false)
     }
+
+    return state
 }
